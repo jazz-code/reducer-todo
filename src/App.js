63880858946ec27197ae840615412f1e59908a9d
jazz-code <ren.meta@gmail.com>
2019-08-13 19:05:57 -0400

    passing the value of input

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ import "./App.css";
 function App() {
   const inputRef = useRef();
   const [items, dispatch] = useReducer((state, action) => state, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //dispatch an action to add item to list
+    // passes the value of the input
+    dispatch({
+      type: "add",
+      name: inputRef.current.value
+    });
+    //resets form
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="App">
-      <input ref={inputRef} />
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} />
+      </form>
       <ul>
         {items.map(item => (
           <li key={item.id}>{item.nam}</li>
