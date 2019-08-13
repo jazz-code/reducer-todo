be0e0b4203847a06e4c433ed0acfb4ee4f84eb85
jazz-code <ren.meta@gmail.com>
2019-08-13 15:38:32 -0400

    Build a simple reducer and initial state

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,11 @@
+export const initalState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589
+};
+
+export const todoReducer = (state, action) => {
+  console.log("todoReducer", state, action);
+
+  return state;
+};
