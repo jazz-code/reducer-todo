9918131e38a5184e6dacaa14fdcdb5c399cc080b
ant <ren.meta@gmail.com>
2019-08-21 18:31:18 -0400

    Animations and more styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { useReducer, useRef } from "react";
+import React, { useReducer, useRef, useEffect, useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+// import { TimelineLite, CSSPlugin } from "gsap/all";
+// import { TweenMax } from "gsap";
+// import TimelineMax from "gsap/TimelineMax";
+// import TweenLite from "gsap/TweenLite";
+
+import {Animated} from "react-animated-css";
+
 import "./styles/App.scss";
 
+
 function App() {
+
   const inputRef = useRef();
   const [items, dispatch] = useReducer((state, action) => {
     switch (action.type) {
@@ -34,8 +43,15 @@ function App() {
     inputRef.current.value = "";
   };
 
+
+
   return (
+    <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
     <div className="App">
+       {/* <button onClick={() => animation.play()}>Play</button>
+       <button ref={element => {
+            ref1 = element;
+          }} onClick={() => animation.pause()}>Pause</button> */}
       <h1>Make a to-do list!</h1>
       <form className="form" onSubmit={handleSubmit}>
         <input className="input" ref={inputRef} placeholder="Enter a Todo" />
@@ -43,23 +59,30 @@ function App() {
       </form>
       <ul className="list">
         {items.map((item, index) => (
+          <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
           <li className="list-item" key={item.id}>
             {item.name}
             <button 
               className="btn-remove"
+              
               onClick={
                 //dispatch an action called "remove" at the index of the item
+                
                 () => dispatch({ type: "remove", index })
               }
             >
-              Delete
+              Done
             </button>
-          </li>
+          </li>  
+          </Animated>
         ))}
+      
       </ul>
+     
       {/* <TodoForm />
       <TodoList /> */}
     </div>
+    </Animated>
   );
 }
 
